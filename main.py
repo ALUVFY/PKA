72e8bb027aa54ea89ca93e5989f0c2f91a273ba8
AluvFy <2859097652@qq.com>
2026-05-25 00:40:13 +0800

    文件上传读取和内容预览

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
 import streamlit as st
+from docx import Document
+from langchain_community.document_loaders import PyPDFLoader
+import tempfile
+import os
 
 st.title("我的AI知识助手")
 
 uploaded_file = st.file_uploader(
-    "上传文件"
+    "上传文件",
+    type=["pdf","docx"]
 )
 
 if uploaded_file:
-    st.write(
-        f"文件名:{uploaded_file.name}"
+
+    suffix=uploaded_file.name.split(".")[-1]
+
+    with tempfile.NamedTemporaryFile(
+            delete=False,
+            suffix="."+suffix
+    ) as tmp:
+
+        tmp.write(uploaded_file.read())
+
+        temp_path=tmp.name
+
+    text=""
+
+    if suffix=="pdf":
+
+        loader=PyPDFLoader(temp_path)
+
+        docs=loader.load()
+
+        text="\n".join(
+            [doc.page_content for doc in docs]
+        )
+
+    elif suffix=="docx":
+
+        doc=Document(temp_path)
+
+        text="\n".join(
+            [p.text for p in doc.paragraphs]
+        )
+
+    st.success("文件读取成功")
+
+    st.text_area(
+        "文件内容预览",
+        text,
+        height=300
     )
+
+    os.remove(temp_path)
